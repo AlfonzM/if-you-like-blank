3e11d985529cf05f9e9940088d749da8147e7486
Alfonz <m.alfonz@gmail.com>
2015-05-31 01:35:45 +0800

    add new features: related artist search, play top track, play next track, get new related artist

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
-	var fetchArtistResults = function(callback){
+	var audioObject;
+	var previewSongNum;
+	var relatedArtists;
+	var currentTracks;
+	var currentRelatedArtist;
+
+	var fetchArtistSearchResults = function(callback){
 		var query = $("#query").val();
 
-		console.log("get artist list");
 		$.ajax({
 	        url: 'https://api.spotify.com/v1/search',
 	        data: {
@@ -16,38 +21,90 @@ $(document).ready(function(){
 	    });
 	}
 
-	var searchArtist = function(){
-		results = document.getElementById('results');
-		results.innerHTML = "";
+	var selectArtist = function(artistId){
+		previewSongNum = 0;
 
-	    fetchArtistResults(function(response){
-			for(i = 0 ; i < response.artists.items.length; i++){
-       			results.innerHTML += response.artists.items[i].name + "<br>";
-        	}
+		// get the artist
+		$.ajax({
+	        url: 'https://api.spotify.com/v1/artists/' + artistId,
+
+	        success: function (artist) {
+	        	if("images" in artist && artist.images[0] != undefined){
+	        		$("#artist-photo").attr("src", artist.images[0].url);
+	        	}
+	        	else{
+	        		$("#artist-photo").attr("src", "");
+	        	}
+	        }
 	    });
 
-		$("#query").autocomplete("close");
-	};
+		// get this artist's top tracks and play it
+		$.ajax({
+	        url: 'https://api.spotify.com/v1/artists/' + artistId + "/top-tracks",
+	        data: {
+	        	country: 'PH'
+	        },
+
+	        success: function (toptracks) {
+	        	currentTracks = toptracks.tracks;
+	        	playSong();
+	        }
+	    });
+	}
+
+	var selectRelatedArtist = function(){
+		var artist = relatedArtists[currentRelatedArtist];
+    	selectArtist(artist.id);
+    	console.log("current playing: " + artist.id);
+    	$("#artist-name").html(artist.name);
+	}
+
+	var playSong = function(){
+    	if(audioObject){
+    		audioObject.pause();
+    	}
+    	audioObject = new Audio(currentTracks[previewSongNum].preview_url);
+        audioObject.play();
+
+        $("#now-playing").html("Now playing: " + currentTracks[previewSongNum].name);
+	}
+
+	$("#play-another-song").click(function(){
+    	audioObject.pause();
+
+		previewSongNum++;
+		playSong();
+	});
+
+	$("#select-another-artist").click(function(){
+		currentRelatedArtist++;
+		selectRelatedArtist();
+	});
 
 	$("#query").autocomplete({
 		source: function(request, response){
-					fetchArtistResults(function(data){
+					fetchArtistSearchResults(function(data){
 			        	var results = [];
 			        	for(i = 0 ; i < data.artists.items.length; i++){
-			       			results.push(data.artists.items[i].name);
+			       			results.push({artistId: data.artists.items[i].id, value: data.artists.items[i].name});
 			        	}
 			        	response(results);
 					});
 				},
-		select: function(){
-			searchArtist();
+		select: function(event, ui){
+	        $("#selected-artist-name").html("Artsits similar to " + ui.item.value + ":");
+			console.log("Selected: " + ui.item.artistId);
+
+	        $.ajax({
+		        url: 'https://api.spotify.com/v1/artists/' + ui.item.artistId + '/related-artists',
+
+		        success: function (data) {
+		        	relatedArtists = data.artists;
+		        	currentRelatedArtist = 0;
+		        	selectRelatedArtist();
+		        }
+		    });
+			return false;
 		}
 	});
-
-	$("#search-artist").submit(function(e){
-		e.preventDefault();
-		searchArtist();
-		return false;
-	});
-
 });
